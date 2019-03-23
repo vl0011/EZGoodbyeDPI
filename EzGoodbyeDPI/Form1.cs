b0eedc3db3293d9044bdff78ec4800800c435748
vl0011 <vl0114@naver.com>
2019-03-23 22:57:18 +0900

    .
    .

---
+++ EzGoodbyeDPI/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace EzGoodbyeDPI
+{
+    public partial class Form1 : Form
+    {
+        ~Form1()
+        {
+            button2_Click(null, null);
+        }
+        ProcessStartInfo pinfo;
+        Process process = new Process();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (Process p in Process.GetProcesses())
+            {
+                if (p.ProcessName.StartsWith("goodbyedpi"))
+                {
+                    MessageBox.Show("이미 GoodbyeDPI가 실행중입니다. 종료해주세요.");
+                    return;
+                }
+            }
+            button1.Enabled = false;
+            trackBar_lv.Enabled = false;
+            try
+            {
+                if (Environment.Is64BitOperatingSystem == true)
+                {
+                    pinfo = new ProcessStartInfo(Application.StartupPath + "\\x86_64\\goodbyedpi.exe", "-" + trackBar_lv.Value);
+
+                }
+                else
+                {
+                    pinfo = new ProcessStartInfo(Application.StartupPath + "\\x86\\goodbyedpi.exe", "-" + trackBar_lv.Value);
+                }
+                pinfo.CreateNoWindow = true;
+                pinfo.WindowStyle = ProcessWindowStyle.Hidden;
+                pinfo.UseShellExecute = false;
+                pinfo.RedirectStandardOutput = true;
+                process.StartInfo = pinfo;
+                process.Start();
+                MessageBox.Show("우회 시작중");
+            }
+            catch (Win32Exception ex)
+            {
+                button1.Enabled = true;
+                trackBar_lv.Enabled = true;
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach(Process p in Process.GetProcesses())
+            {
+                if (p.ProcessName.StartsWith("goodbyedpi"))
+                {
+                    p.Kill();
+                }
+            }
+            button1.Enabled = true;
+            trackBar_lv.Enabled = true;
+        }
+    }
+}
---
+++ EzGoodbyeDPI/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Security.Principal;
+
+namespace EzGoodbyeDPI
+{
+    static class Program
+    {
+        /// <summary>
+        /// 해당 응용 프로그램의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ///
+            if (IsAdministrator() == false)
+            {
+                try
+                {
+                    ProcessStartInfo procInfo = new ProcessStartInfo();
+                    procInfo.UseShellExecute = true;
+                    procInfo.FileName = Application.ExecutablePath;
+                    procInfo.WorkingDirectory = Environment.CurrentDirectory;
+                    procInfo.Verb = "runas";
+                    Process.Start(procInfo);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+
+                return;
+            }
+            ///
+
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+        public static bool IsAdministrator()
+        {
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+
+            if (null != identity)
+            {
+                WindowsPrincipal principal = new WindowsPrincipal(identity);
+                return principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+
+            return false;
+        }
+    }
+}
